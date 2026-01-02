1fdbb55bfbed6b74e4cdb43214556c7c7ff19bde
Angie <Angiebp115200@gmail.com>
2026-01-02 01:31:53 -0500

    Realizando ajuste de diseño 02/01/2026

--- portafolio/src/components/home/homebody.jsx
+++ portafolio/src/components/home/homebody.jsx
@@ -7,19 +7,31 @@ import yoImg from '../../components/img/yp.jpg'
 export const BodyHome = () => {
   return (
     <Fragment>
-    <section className="body-home" id="inicio">
-      <div className="body-home__container1">
-        <div className="body-home_subcontainer1">
-          <h1>Desarrolladora Full Stack</h1>
-          <h2>Con preferencia en desarrollo Frontend</h2>
-         <button className="btn btn-primary">Ver proyectos</button><br />
-         <img src={tecnologias} alt="tecnologias usadas" />
+        <section className='body-home'>
+          <div className="body-home__container1">
+            <h1>Desarrolladora Full Stack</h1>
+            <p>Soy desarrolladora full stack con afinidad por el frontend y la 
+            experiencia de usuario.<br></br> Disfruto crear interfaces bien pensadas, 
+            cuidar los detalles visuales y comprender el propósito 
+            detrás de cada proyecto<br></br> para construir soluciones funcionalesy claras.
+            </p>
+            <div className="git">
+               <button className="btn btn-primary">GIT</button><br />
+              <button className="btn btn-primary">Ver proyectos</button><br />
+            </div>
+            
+           </div>
+         
+          <div className="about-me1">
+            <div className="imgYo">
+            <img src={yoImg} alt="" />
+          </div>
         </div>
-        <div className="body-home_subcontainer2">
-          <img src={animeYo} alt="Ilustración de personaje programando" />
-        </div> 
-      </div>
-    </section>
+  
+        </section>
+      
+    
+
     <div className="wave-separator">
   <svg viewBox="0 0 500 150" preserveAspectRatio="none">
     <path
@@ -28,16 +40,7 @@ export const BodyHome = () => {
          L500.00,0.00 L0.00,0.00 Z" />
   </svg>
 </div>
-    <section className='body-home2'>
-      <div className="about-me1">
-        <div className="imgYo">
-          <img src={yoImg} alt="" />
-        </div>
-      </div>
-        <div className="about-me2">
-            <p>Soy desarrolladora full stack con afinidad por el frontend y la experiencia de  usuario. Disfruto construir interfaces bien pensadas, cuidar los detalles visuales y entender el proposito detras de cada proyecto. Me motiva aprender constantemente, mejorar mis habilidades y enfrentar nuevos desafíos como una oportunidad de crecimiento personal y profesional, soy una persona autodidacta que quiere aprender cada dia más y más cosas </p>
-        </div>
-    </section>
+    
     </Fragment>
   );
 };
